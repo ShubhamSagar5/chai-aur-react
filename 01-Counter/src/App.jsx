19c8c2b87d154fbf23a7a92952425c89c7386433
shubham <shubhamsagar628@gmail.com>
2024-04-14 13:51:18 +0530

    first app

---
+++ 01-Counter/src/App.jsx
@@ -0,0 +1,35 @@
+const { useState } = require("react")
+
+
+const App = () => {
+   
+let [counter,setCounter] = useState(1)
+
+const addValue = () => {
+    if(counter <= 20){
+        setCounter(counter+1)
+    }else{
+        alert("You Not Add Value More Than 20")
+    }
+}
+
+const removeValue = () => {
+    if(counter > 0){
+        setCounter(counter-1)
+    }else{
+        alert("You Not Remove Value after zero ")
+    }
+}
+
+    return (
+        <div>
+                <h1>Chai Aur React -- Counter APP</h1>
+                <h2>{counter}</h2>
+
+                <button onClick={addValue}>Add +</button>
+                <br />
+                <br />
+                <button onClick={removeValue}>Remove -</button>
+        </div>
+    )
+}
